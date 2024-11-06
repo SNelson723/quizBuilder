31bb2d9bbd9d17a552d7946334a03a842ea932c8
SNelson723 <stephenn649@gmail.com>
2024-11-06 17:20:54 -0600

    Added the beginning of the NavBar so I can start building out the other pages and navigate

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,22 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
   return (
-    <div>
-      Nav Bar
+    <div id='navBar'>
+      <Navbar bg="dark" data-bs-theme="dark" className='h-100'>
+        <Container>
+          <Navbar.Brand as="span" to="/home">Quizzia</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/" id="homeLink">Home</Nav.Link>
+            {/* <Nav.Link as={Link} to="/designs" id="designsLink">Designs</Nav.Link>
+            <Nav.Link as={Link} to="/about" id="aboutLink">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact" id="contactLink">Contact</Nav.Link> */}
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 };
 
 export default NavBar;
