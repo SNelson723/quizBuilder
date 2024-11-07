a85595b54bb20e912a8ffecb2269554847b1ac5e
SNelson723 <stephenn649@gmail.com>
2024-11-06 18:19:05 -0600

    Using axios to get the quiz categories for the front end, gonna start building out the choose your quiz card

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-// import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import NavBar from './NavBar.jsx';
 import Home from './Home.jsx';
 import QuizGame from './QuizGame/QuizGame.jsx';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, Outlet } from "react-router-dom";
+import axios from 'axios'
 
 const App = () => {
+  const [categories, setCategories] = useState([]);
+  const queryLink = import.meta.env.VITE_TRIVIA_API;
+  const api_key = import.meta.env.VITE_API_KEY;
+  const baseUrl = queryLink + api_key;
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const {data} = await axios.get('https://opentdb.com/api_category.php');
+      setCategories(data.trivia_categories);
+    };
+    fetchCategories();
+  }, []);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<Home />} />
-        <Route path="game" element={<QuizGame />} />
+        <Route path="game" element={<QuizGame quizUrl={baseUrl} categories={categories} />} />
       </Route>
     )
   );
--- src/QuizGame/QuizGame.jsx
+++ src/QuizGame/QuizGame.jsx
@@ -1,15 +1,29 @@
+/* eslint-disable react/prop-types */
+import Form from 'react-bootstrap/Form';
 
+const QuizGame = ({ quizUrl, categories}) => {
+  /**
+   * There will be quiz cards
+   * Options for quizzes?
+   * Let the use know if they got the answer right/wrong and display a prompt with a random message
+   */
+  console.log(categories)
 
-const QuizGame = () => {
-  const queryLink = import.meta.env.VITE_TRIVIA_API;
-  const api_key = import.meta.env.VITE_API_KEY;
-  const baseUrl = queryLink + api_key;
   return (
     <>
       <h1 className="text-center">QuizGame</h1>
-      {/* <h5>{baseUrl}</h5> */}
+      <Form>
+        <Form.Group>
+          <Form.Label>Categories</Form.Label>
+          <Form.Select>
+            <option>Select your quizzia ground!</option>
+            {categories.map(cat => <option key={cat.name + cat.id}>{cat.name}</option>)}
+          </Form.Select>
+        </Form.Group>
+      </Form>
     </>
   );
 };
+
 
 export default QuizGame;
