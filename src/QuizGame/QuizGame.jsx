6176626aa2382be4b949ecd998b6ed726e4d2b4c
SNelson723 <stephenn649@gmail.com>
2024-11-08 11:03:21 -0600

    Added state variables to QuizGame.jsx to build the query string to imporve the controlled behavior

--- src/QuizGame/QuizGame.jsx
+++ src/QuizGame/QuizGame.jsx
@@ -1,43 +1,44 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Form, Container, Card, Button }from 'react-bootstrap';
 import axios from 'axios';
 
 const QuizGame = ({ categories }) => {
-  const [url, setUrl] = useState(import.meta.env.VITE_TRIVIA_API + import.meta.env.VITE_API_KEY);
+  const [url, setUrl] = useState(import.meta.env.VITE_TRIVIA_API);
+  const [difficulty, setDifficulty] = useState('');
+  const [category, setCategory] = useState('');
+  const [type, setType] = useState('');
   /**
    * There will be quiz cards
    * Options for quizzes?
    * Let the use know if they got the answer right/wrong and display a prompt with a random message
+   *  + import.meta.env.VITE_API_KEY
    */
-  // const finalUrl = quizUrl;
-  // console.log(finalUrl);
-  const handleDifficultySelect = (e) => {
-
-  };
+  // console.log(categories)
 
   return (
     <>
       <h1 className="text-center">QuizGame</h1>
+      <p>{url}</p>
       <Container className="w-50">
         <Card>
           <Form className='p-2'>
             <Form.Group className='d-flex'>
               <Form.Group  className='w-50'>
                 <Form.Label htmlFor='categories'>Categories</Form.Label>
-                <Form.Select id="categories">
+                <Form.Select id="categories" onChange={(e) => handleOnSelect(e)}>
                   <option>Select your quizzia ground!</option>
-                  {categories.map(cat => <option key={cat.name + cat.id}>{cat.name}</option>)}
+                  {categories.map(cat => <option key={cat.name + cat.id} id={cat.id}>{cat.name}</option>)}
                 </Form.Select>
               </Form.Group>
 
               <Form.Group>
                 <Form.Label htmlFor="difficulties">Difficulty</Form.Label>
-                <Form.Select>
-                  <option>Any Difficulty</option>
-                  <option>Easy</option>
-                  <option>Medium</option>
-                  <option>Hard</option>
+                <Form.Select id="difficulties" onChange={(e) => handleOnSelect(e)}>
+                  <option id="">Any Difficulty</option>
+                  <option id="easy">Easy</option>
+                  <option id="medium">Medium</option>
+                  <option id="hard">Hard</option>
                 </Form.Select>
               </Form.Group>
             </Form.Group>
