dd9db288fcc7e998c27c6f54e7ebdec30d90cd72
SNelson723 <stephenn649@gmail.com>
2024-11-08 11:36:55 -0600

    Adding more fields to the form so I can finish building the url in QuizGame. Just need to add type then move on to setting up my server and endpoint to handle the request on the backend

--- src/QuizGame/QuizGame.jsx
+++ src/QuizGame/QuizGame.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const QuizGame = ({ categories }) => {
   const [url, setUrl] = useState(import.meta.env.VITE_TRIVIA_API);
+  const [amount, setAmount] = useState('');
   const [difficulty, setDifficulty] = useState('');
   const [category, setCategory] = useState('');
   const [type, setType] = useState('');
@@ -13,35 +14,45 @@ const QuizGame = ({ categories }) => {
    * Options for quizzes?
    * Let the use know if they got the answer right/wrong and display a prompt with a random message
    *  + import.meta.env.VITE_API_KEY
+   * 
+   * amount => category => difficulty => type => api key
    */
   // console.log(categories)
+  const handleOnSelect = (e, setOption) => {
+    const index = e.target.selectedIndex;
+    const value = e.target.children[index].id;
+    setOption(value);
+  };
+  console.log(difficulty, category, amount)
 
   return (
     <>
       <h1 className="text-center">QuizGame</h1>
       <p>{url}</p>
       <Container className="w-50">
-        <Card>
-          <Form className='p-2'>
-            <Form.Group className='d-flex'>
-              <Form.Group  className='w-50'>
+        <Card className='p-2'>
+          <Form id="quizOptionForm">
+            <Form.Group className='w-25 mx-auto'>
+              <Form.Label>Number of Questions</Form.Label>
+              <Form.Control type="number" value={amount} onChange={(e) => setAmount(e.target.value)}></Form.Control>
+            </Form.Group>
+              <Form.Group  className='w-50 mx-auto text-center'>
                 <Form.Label htmlFor='categories'>Categories</Form.Label>
-                <Form.Select id="categories" onChange={(e) => handleOnSelect(e)}>
-                  <option>Select your quizzia ground!</option>
+                <Form.Select id="categories" onChange={(e) => handleOnSelect(e, setCategory)}>
+                  <option id="">Select your quizzia ground!</option>
                   {categories.map(cat => <option key={cat.name + cat.id} id={cat.id}>{cat.name}</option>)}
                 </Form.Select>
               </Form.Group>
 
-              <Form.Group>
+              <Form.Group className='w-50 mx-auto text-center'>
                 <Form.Label htmlFor="difficulties">Difficulty</Form.Label>
-                <Form.Select id="difficulties" onChange={(e) => handleOnSelect(e)}>
+                <Form.Select id="difficulties" onChange={(e) => handleOnSelect(e, setDifficulty)}>
                   <option id="">Any Difficulty</option>
                   <option id="easy">Easy</option>
                   <option id="medium">Medium</option>
                   <option id="hard">Hard</option>
                 </Form.Select>
               </Form.Group>
-            </Form.Group>
           </Form>
           <Button className='w-25 mx-auto'>Get your quiz on</Button>
         </Card>
