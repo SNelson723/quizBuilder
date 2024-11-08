297393c37d7ef4094f6bf989c74fca24ed6d7eeb
SNelson723 <stephenn649@gmail.com>
2024-11-08 11:40:08 -0600

    Moved the form fields around to look cleaner in QuizGame.jsx

--- src/QuizGame/QuizGame.jsx
+++ src/QuizGame/QuizGame.jsx
@@ -32,27 +32,30 @@ const QuizGame = ({ categories }) => {
       <Container className="w-50">
         <Card className='p-2'>
           <Form id="quizOptionForm">
-            <Form.Group className='w-25 mx-auto'>
+
+            <Form.Group className='w-25 mx-auto text-center'>
               <Form.Label>Number of Questions</Form.Label>
               <Form.Control type="number" value={amount} onChange={(e) => setAmount(e.target.value)}></Form.Control>
             </Form.Group>
-              <Form.Group  className='w-50 mx-auto text-center'>
-                <Form.Label htmlFor='categories'>Categories</Form.Label>
-                <Form.Select id="categories" onChange={(e) => handleOnSelect(e, setCategory)}>
-                  <option id="">Select your quizzia ground!</option>
-                  {categories.map(cat => <option key={cat.name + cat.id} id={cat.id}>{cat.name}</option>)}
-                </Form.Select>
-              </Form.Group>
 
-              <Form.Group className='w-50 mx-auto text-center'>
-                <Form.Label htmlFor="difficulties">Difficulty</Form.Label>
-                <Form.Select id="difficulties" onChange={(e) => handleOnSelect(e, setDifficulty)}>
-                  <option id="">Any Difficulty</option>
-                  <option id="easy">Easy</option>
-                  <option id="medium">Medium</option>
-                  <option id="hard">Hard</option>
-                </Form.Select>
-              </Form.Group>
+            <Form.Group className='w-25 mx-auto text-center'>
+              <Form.Label htmlFor="difficulties">Difficulty</Form.Label>
+              <Form.Select id="difficulties" onChange={(e) => handleOnSelect(e, setDifficulty)}>
+                <option id="">Any Difficulty</option>
+                <option id="easy">Easy</option>
+                <option id="medium">Medium</option>
+                <option id="hard">Hard</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group  className='w-50 mx-auto text-center'>
+              <Form.Label htmlFor='categories'>Categories</Form.Label>
+              <Form.Select id="categories" onChange={(e) => handleOnSelect(e, setCategory)}>
+                <option id="">Select your quizzia ground!</option>
+                {categories.map(cat => <option key={cat.name + cat.id} id={cat.id}>{cat.name}</option>)}
+              </Form.Select>
+            </Form.Group>
+
           </Form>
           <Button className='w-25 mx-auto'>Get your quiz on</Button>
         </Card>
