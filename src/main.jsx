4be9fc749b03cd9a163603f4304d34d9d7247cee
SNelson723 <stephenn649@gmail.com>
2024-11-24 18:24:02 -0600

    Working on adding google oauth so I can create accounts and work on the profile\

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './main.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
     <App />
   </StrictMode>,
 )
