0eb51baffb4eb6506282cd155603c6490b9da893
SNelson723 <stephenn649@gmail.com>
2024-11-26 18:32:27 -0600

    Working on adding a Google OAuth client

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './main.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
-    <App />
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>;
   </StrictMode>,
 )
