bdcf2a59d294ab3edca4ad8fb117a4aae8070d5b
SNelson723 <stephenn649@gmail.com>
2024-11-27 18:40:23 -0600

    Finished the basic layout of the login card in Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,9 +9,13 @@ const Login = () => {
   return (
   <div id="loginMain" className='pt-5'>
     <Container className='w-50 d-flex justify-content-center mt-5'>
-      <Card className='w-50'>
-        <img src={quizziaLogo} />
-        <Button className='w-50 mx-auto mb-3' onClick={authHandle}>Google</Button>
+      <Card className='w-50' >
+        <img src={quizziaLogo}/>
+        <p className='text-center' style={{fontFamily: 'Stencil Std', fontSize: '2rem'}}>
+          Get your quiz on
+        </p>
+        <p className='text-center' style={{fontFamily: 'Stencil Std', fontSize: '1.5rem'}}>Login With Google</p>
+        <Button className='w-50 mx-auto mb-3' onClick={authHandle}>Login</Button>
       </Card>
     </Container>
   </div>
