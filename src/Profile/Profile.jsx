cbd29c11d9f4d27ee1c86019a04a65876bbc05bb
SNelson723 <stephenn649@gmail.com>
2024-11-27 19:06:14 -0600

    Styling the profile header currently

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,22 +1,27 @@
 // Profile.jsx
 import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { Container, Card, Button } from 'react-bootstrap';
 
 const Profile = () => {
   const user = useLoaderData();
-  console.log(user[0]);
+  const { userName, image_url, } = user[0];
+  // console.log(user[0]);
 
   if (!user) {
     return <div>Loading...</div>; // Show loading state while fetching
   }
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      <p>Name: {user[0].firstName} {user[0].lastName}</p>
-      <p>Email: {user[0].email}</p>
-      {/* Add more fields as necessary */}
-    </div>
+    <Container>
+      <Card className='d-flex mt-4' style={{width: '35vw'}}>
+        <img src={image_url} style={{borderRadius: '10px', border: '3px solid grey', width: '9rem'}} />
+        <div className='ms-4 mt-3'>
+          <p id='profileName'>{userName}</p>
+          <h6 style={{color: 'grey'}}>0 Friends</h6>
+        </div>
+      </Card>
+    </Container>
   );
 };
 
