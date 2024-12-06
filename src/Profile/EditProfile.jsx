969e77aa2942ced9b1733bf9f7a4514f4e762edd
SNelson723 <stephenn649@gmail.com>
2024-12-06 16:58:50 -0600

    Almost done with setting up the edit profile form. About to make the endpoint to update the user profile

--- src/Profile/EditProfile.jsx
+++ src/Profile/EditProfile.jsx
@@ -11,23 +11,21 @@ const EditProfile = ({ userId, setBio, setFavoriteGenre, setOccupation, setEdit 
     // axios.put()
   };
 
-  const handleOnChange = (e) => {
-
-  };
+  console.log(bioText, genreText, occupationText);
 
   return (
     <>
       <Container className='w-50 mt-5'>
-        <Card className='w-100'>
+        <Card style={{backgroundColor: '#eef3f9'}} className='w-100'>
           <Card.Header className='d-flex justify-content-between' style={{backgroundColor: '#b3cde4'}}>
             <div className='my-auto ms-1 fs-5'>Edit Profile</div>
-            <Button className='rounded-circle text-black' style={{backgroundColor: '#b3cde4', borderColor: '#b3cde4'}} onClick={() => setEdit(false)}>X</Button>
+            <Button id='closeEditBtn' className='rounded-circle text-black' style={{backgroundColor: '#b3cde4', borderColor: '#b3cde4'}} onClick={() => setEdit(false)}>X</Button>
           </Card.Header>
           <Card.Body style={{backgroundColor: '#eef3f9'}}>
             <Form className='my-2'>
               <Form.Group className='w-75 mx-auto'>
                 <Form.Label>Bio:</Form.Label>
-                <Form.Control type='text' onChange={setBioText} />
+                <Form.Control id='bio' type='text' onChange={setBioText} />
               </Form.Group>
 
               <Form.Group className='w-75 mx-auto'>
@@ -41,6 +39,7 @@ const EditProfile = ({ userId, setBio, setFavoriteGenre, setOccupation, setEdit 
               </Form.Group>
             </Form>
           </Card.Body>
+          <Button className='w-50 mx-auto mb-4' style={{backgroundColor: '#537692'}}>Save Changes</Button>
         </Card>
       </Container>
     </>
