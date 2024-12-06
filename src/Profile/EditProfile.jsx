a1f689327c8eb929729a026d51383b1bf5c302e7
SNelson723 <stephenn649@gmail.com>
2024-12-06 16:47:57 -0600

    Adding the color paletter to make it easier to see the controlled form in EditProfile.jsx

--- src/Profile/EditProfile.jsx
+++ src/Profile/EditProfile.jsx
@@ -19,26 +19,28 @@ const EditProfile = ({ userId, setBio, setFavoriteGenre, setOccupation, setEdit 
     <>
       <Container className='w-50 mt-5'>
         <Card className='w-100'>
-          <Card.Header className='d-flex justify-content-between'>
-            <div>Edit Profile</div>
-            <Button className='rounded-circle' onClick={() => setEdit(false)}>X</Button>
+          <Card.Header className='d-flex justify-content-between' style={{backgroundColor: '#b3cde4'}}>
+            <div className='my-auto ms-1 fs-5'>Edit Profile</div>
+            <Button className='rounded-circle text-black' style={{backgroundColor: '#b3cde4', borderColor: '#b3cde4'}} onClick={() => setEdit(false)}>X</Button>
           </Card.Header>
-          <Form>
-            <Form.Group className='w-75 mx-auto'>
-              <Form.Label>Bio:</Form.Label>
-              <Form.Control type='text' onChange={setBioText} />
-            </Form.Group>
+          <Card.Body style={{backgroundColor: '#eef3f9'}}>
+            <Form className='my-2'>
+              <Form.Group className='w-75 mx-auto'>
+                <Form.Label>Bio:</Form.Label>
+                <Form.Control type='text' onChange={setBioText} />
+              </Form.Group>
 
-            <Form.Group className='w-75 mx-auto'>
-              <Form.Label>Favorite Genre:</Form.Label>
-              <Form.Control id='genre' type='text' onChange={(setGenreText)} />
-            </Form.Group>
+              <Form.Group className='w-75 mx-auto'>
+                <Form.Label>Favorite Genre:</Form.Label>
+                <Form.Control id='genre' type='text' onChange={(setGenreText)} />
+              </Form.Group>
 
-            <Form.Group className='w-75 mx-auto'>
-              <Form.Label>Occupation:</Form.Label>
-              <Form.Control id='occupation' type='text' onChange={setOccupationText} />
-            </Form.Group>
-          </Form>
+              <Form.Group className='w-75 mx-auto'>
+                <Form.Label>Occupation:</Form.Label>
+                <Form.Control id='occupation' type='text' onChange={setOccupationText} />
+              </Form.Group>
+            </Form>
+          </Card.Body>
         </Card>
       </Container>
     </>
